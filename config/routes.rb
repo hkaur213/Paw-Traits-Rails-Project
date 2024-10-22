06cbbd8382f60e1bf63ea61b08d3e86ba90458e2
Harleen Kaur <hharleenkaur@academic.rrc.ca>
2024-10-22 10:58:23 -0500

    Sub-Breeds created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "pages/about"
   root 'home#index'
   
   get 'about', to: 'home#about'
--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@ def generate_dog_description
     "These dogs are often very loyal and protective of their owners.",
     "Their beautiful coat comes in various colors and patterns, making them quite unique."
   ]
-  
+
   sentences.sample(rand(3..4)).join(' ') # Randomly select 3-4 sentences and join them
 end
 
@@ -26,9 +26,41 @@ breeds.each do |breed, sub_breeds|
   breed_record = Breed.create(name: breed)
 
   # Create sub-breed records and store them in an array
-  sub_breed_records = sub_breeds.map do |sub_breed|
-    SubBreed.create(name: sub_breed, breed: breed_record, description: generate_dog_description)
+sub_breed_records = sub_breeds.map do |sub_breed|
+  sub_breed_record = SubBreed.create(name: sub_breed, breed: breed_record, description: generate_dog_description)
+
+  # Fetch and attach an image for each sub-breed
+  begin
+    sub_image_response = HTTParty.get("https://dog.ceo/api/breed/#{breed}/#{sub_breed}/images/random")
+    sub_image_url = sub_image_response.parsed_response['message']
+
+    # Attach the sub-breed image if it exists
+    if sub_breed_record.persisted? && sub_image_url
+      sub_image_file = HTTParty.get(sub_image_url)
+      io = StringIO.new(sub_image_file.body)
+      io.class.class_eval { attr_accessor :original_filename, :content_type }
+      io.original_filename = "#{sub_breed}.jpg"
+      io.content_type = 'image/jpg'
+
+      # Resize and attach the image (similar to dogs)
+      image = MiniMagick::Image.read(io)
+      image.resize "300x300"
+      resized_io = StringIO.new
+      image.write(resized_io)
+      resized_io.rewind
+
+      # Attach the resized image to the sub-breed
+      sub_breed_record.image.attach(io: resized_io, filename: io.original_filename, content_type: io.content_type)
+      puts "Attached image for sub-breed: #{sub_breed_record.name}"
+    else
+      puts "No image URL found for sub-breed: #{sub_breed}"
+    end
+  rescue => e
+    puts "Error processing image for sub-breed #{sub_breed}: #{e.message}"
   end
+
+  sub_breed_record
+end
 
   # Seed dogs with images and descriptions
   10.times do
@@ -42,28 +74,27 @@ breeds.each do |breed, sub_breeds|
       name: Faker::Creature::Dog.name,
       age: rand(1..15),
       breed_id: breed_record.id,
-      sub_breed_id: sub_breed_record&.id, # This will be nil if no sub-breed is selected
-      image_url: image_url,                # Add the image URL here
-      description: generate_dog_description # Add a fake multi-sentence description
+      sub_breed_id: sub_breed_record&.id,
+      description: generate_dog_description
     )
 
     # Attach image if the dog is persisted
     if dog.persisted?
-      image_file = HTTParty.get(image_url) # Fetch the image
-      io = StringIO.new(image_file.body)    # Wrap the body in a StringIO object
-      io.class.class_eval { attr_accessor :original_filename, :content_type } # Add attributes
+      image_file = HTTParty.get(image_url)
+      io = StringIO.new(image_file.body)
+      io.class.class_eval { attr_accessor :original_filename, :content_type }
       io.original_filename = "#{dog.name}.jpg"
       io.content_type = 'image/jpg'
 
       # Resize the image using MiniMagick
       begin
         image = MiniMagick::Image.read(io)
-        image.resize "300x300" # Set your desired size here (e.g., 300x300)
+        image.resize "300x300"
         resized_io = StringIO.new
-        image.write(resized_io) # Write resized image to StringIO
-        resized_io.rewind # Rewind the IO
+        image.write(resized_io)
+        resized_io.rewind
 
-        # Attach the resized image
+        # Attach the resized image to the dog
         dog.image.attach(io: resized_io, filename: io.original_filename, content_type: io.content_type)
       rescue => e
         puts "Error processing image for #{dog.name}: #{e.message}"
