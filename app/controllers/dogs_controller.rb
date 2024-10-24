f4164beb76f69205a7b211e1c1d6048f39d3e0e8
Harleen Kaur <hharleenkaur@academic.rrc.ca>
2024-10-23 23:12:26 -0500

    Search Bar added

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,17 +1,15 @@
 class DogsController < ApplicationController
   def index
-    if params[:search].present? || params[:breed_id].present?
-      @dogs = Dog.all
+    @dogs = Dog.includes(:breed, :sub_breed) # Eager load associations
 
-      if params[:search].present?
-        @dogs = @dogs.where('name LIKE ?', "%#{params[:search]}%")
-      end
-      
-      if params[:breed_id].present? && params[:breed_id].to_i.positive?
-        @dogs = @dogs.where(breed_id: params[:breed_id])
-      end
-    else
-      @dogs = Dog.all
+    if params[:search].present?
+      @dogs = @dogs.where("name LIKE ?", "%#{params[:search]}%")
     end
+
+    if params[:breed_id].present?
+      @dogs = @dogs.where(breed_id: params[:breed_id])
+    end
+
+    Rails.logger.debug "Querying Dogs: #{@dogs.to_sql}"
   end
 end
